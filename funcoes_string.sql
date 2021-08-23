20ce52d5d2a07811d870d2c13aff61207cf59fe3
Rafael Silva <rafael.ssilva03@gmail.com>
2021-08-23 11:28:30 -0300

    add função substring_index

--- funcoes_string.sql
+++ funcoes_string.sql
@@ -16,6 +16,16 @@ WHERE SUBSTR(c_codiprodu,1,3) = '123' AND LENGTH(c_codiprodu) <= 6;
 SELECT SUBSTR(c_razaclien,1,5) Razao_Social, LENGTH(c_codiclien) Tamanho_Cod FROM comclien
 WHERE n_numeclien = 1;
 
+#SUBSTRING_INDEX => Extrai uma substring de uma string antes que um número especificado de delimitador ocorra.
+#SUBSTRING_INDEX(string, delimiter, number)
+#Parâmetros:
+#string: Obrigatório. A string a ser extraida.
+#delimiter: Obrigatório. O delimitador a ser pesquisado.
+#number: Obrigatório. O número de vezes para pesqusisar o delimitador. Pode ser um número positivo ou negativo. Se for um número positivo, esta função
+# retorna tudo à esquerda do delimitador. Se for um número negativo, esta função retorna tudo à direita do delimitador.
+
+SELECT SUBSTRING_INDEX("www.w3schools.com", ".", 2);
+
 #concat()
 SELECT CONCAT(c_codiclien, ' ', c_razaclien ,' ', c_nomeclien) FROM comclien
 WHERE c_razaclien LIKE 'Kik%';
